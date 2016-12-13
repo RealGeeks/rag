289a3345c7996b98e57a77eb93824265485029ed
David Krisch <david@hq.realgeeks.com>
2016-12-12 14:30:27 -1000

    Export country data so it can be accessed outside react-land

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 exports.button = require('./button');
 exports.modal = require('./modal');
 exports.tel = require('./tel');
+exports.countryData = require('./tel/country_data');
 exports.map = require('./map');
 exports.scrollView = require('./scroll-view');
 exports.surface = require('./surface');
