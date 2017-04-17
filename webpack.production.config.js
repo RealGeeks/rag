b569ea140570fb537d38e3e2224846118948f7ef
Keith Raymond <keith@hq.realgeeks.com>
2017-04-17 13:34:52 -1000

    webpack issue

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -33,7 +33,7 @@ module.exports = {
   module: {
     loaders: [
       {
-        test: /.jsx?$/,
+        test: /.js(|x)?$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
